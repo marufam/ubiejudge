8a305cb0286ebdedca18bddfbf6e6528d5cb55a9
Putra Prima <putraprima@gmail.com>
2017-08-20 15:20:01 +0700

    test soal selesai n nambahin dataset

--- common/models/soal.js
+++ common/models/soal.js
@@ -52,16 +52,54 @@ module.exports = function(Soal) {
             //run test jawaban
             //ideally ini pake async paralel 
             function (fileObj,r,testname,callback){
-                exec('cat files/testset/test.txt | files/answer/./'+testname, (error, stdout, stderr) => {
-                    if (error) {
+                async.map(r.testsets,function(data,callback){
+                    exec('cat files/testset/'+data.testName+' | files/answer/./'+testname, (error, stdout, stderr) => {
+                        if (error) {
+                            callback(error);
+                        }else{
+                            // callback(null,stdout);
+                            fs.readFile('files/testset/'+data.answerName, 'utf8', function (err,data) {
+                                if (err) {
+                                    callback(error)
+                                }else{
+                                    if(data.toString()===stdout.toString()){
+                                        callback(null,true);
+                                    }else{
+                                        callback(null,false);
+                                    }
+                                }
+                            });
+                        }
+                    });
+                },function(error,result){
+                    if(error){
                         callback(error);
                     }else{
-                        callback(null,stdout);
+                        // callback(null,fileObj,r,testname,result);
+                        callback(null,result);
                     }
                 });
             }
+            // ,
+            // function(fileObj,r,testname,resultCompile,callback){
+            //     async.map(r.testsets,function(data,callback){
+            //         fs.readFile('files/testset/'+data.answerName, 'utf8', function (err,data) {
+            //             if (err) {
+            //                 callback(error)
+            //             }else{
+            //                 callback(null,data);
+            //             }
+            //           });
+            //     },function(error,result){
+            //         if(error){
+            //             callback(error);
+            //         }else{
+            //             callback(null,result);
+            //         }
+            //     });
+            // }
         ],function(err,res){
-            if(err) callback(err);
+            if(err) cb(err);
             cb(null,res);
         });
     };
